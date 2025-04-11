63aff7e805fc9b1926caba2f22f55aaf875942dd
Matt Main_PC <matmigg0804@gmail.com>
2025-04-11 12:48:55 -0400

    AccuracyRecord RepeatAccuracy PuzzleSet types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,7 +1,33 @@
 export interface User {
-    user_id: number;
-    email: string;
-    pass: string;
-    username?: string;
-    created?: string;
-  }
+  user_id: number;
+  email: string;
+  pass: string;
+  username?: string;
+  created?: string;
+}
+// types.ts
+
+export type AccuracyRecord = {
+  correct: number;
+  incorrect: number;
+};
+
+export type RepeatAccuracy = {
+  repeat: number;
+  correct: number;
+  incorrect: number;
+};
+
+export type PuzzleSet = {
+  set_id: number;
+  name: string;
+  user_id: string;
+  difficulties: string[];
+  size: number;
+  repeats: number;
+  puzzle_ids: string[];
+  repeat_index: number;
+  puzzle_index: number;
+  created_at?: string;
+  updated_at?: string;
+};
