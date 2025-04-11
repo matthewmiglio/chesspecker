61ce05a09ea75a6ccb537c7cd694f06c5eeb0e2b
Matt Main_PC <matmigg0804@gmail.com>
2025-04-11 12:35:00 -0400

    prompt

--- prompt.py
+++ prompt.py
@@ -83,8 +83,8 @@ if __name__ == "__main__":
     file_tree_top_dir = os.getcwd()
 
     files_to_print = [
-        r"H:\my_files\my_programs\chesspecker\src\app\puzzles\page.tsx",
-        r"H:\my_files\my_programs\chesspecker\src\app\api\getSetAccuracy\route.ts",
+        r"H:\my_files\my_programs\chesspecker\src\app\login\page.tsx",
+        r"H:\my_files\my_programs\chesspecker\src\components\chess-board.tsx",
     ]
 
     ignore_folders = [
