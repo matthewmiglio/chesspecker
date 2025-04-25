ad5af61269fd0b26291ae3c4c723288ab7445ce6
Matt Main_PC <matmigg0804@gmail.com>
2025-04-24 23:59:04 -0400

    reverting nextauth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,3 @@
-// app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -10,36 +9,6 @@ const handler = NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  session: {
-    strategy: 'jwt', // Use JWT for session management
-    maxAge: 30 * 24 * 60 * 60, // 30 days
-  },
-  jwt: {
-    secret: process.env.NEXTAUTH_SECRET,
-  },
-  cookies: {
-    sessionToken: {
-      name: `__Secure-next-auth.session-token`,
-      options: {
-        httpOnly: true,
-        sameSite: 'lax',
-        path: '/',
-        secure: process.env.NODE_ENV === 'production',
-      },
-    },
-  },
-  callbacks: {
-    async session({ session, token }) {
-      console.log('Session:', session);
-      console.log('token:', token);
-      return session;
-    },
-    async jwt({ token, user }) {
-      console.log('JWT Token:', token);
-      console.log('JWT user:', user);
-      return token;
-    },
-  },
 });
 
 export { handler as GET, handler as POST };
