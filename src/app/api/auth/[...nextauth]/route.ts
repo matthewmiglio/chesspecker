bb078ea6dc2f98f23d630f1d2943703eff8acb40
Matthew <matmigg0804@gmail.com>
2025-04-28 12:49:39 -0400

    removing old next auth import

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,3 @@
-import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions = {
@@ -15,7 +14,7 @@ export const authOptions = {
         httpOnly: true,
         sameSite: 'lax',
         path: '/',
-        secure: true, 
+        secure: true,
       },
     },
   },
