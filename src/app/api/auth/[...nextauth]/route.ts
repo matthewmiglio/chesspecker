eee80716e1d5feff7932a79b73b5d342353a77c4
Matthew <matmigg0804@gmail.com>
2025-04-28 12:46:19 -0400

    trying to make mobile auth work on safari iphones

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,33 +1,25 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
-const isProduction = process.env.NODE_ENV === "production";
-
-const handler = NextAuth({
+export const authOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_AUTH_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET!,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
-  session: {
-    strategy: "jwt",
-    maxAge: 30 * 24 * 60 * 60, 
-  },
   cookies: {
     sessionToken: {
-      name: `next-auth.session-token`,
+      name: `__Secure-next-auth.session-token`,
       options: {
         httpOnly: true,
-        sameSite: "lax",
-        path: "/",
-        secure: isProduction,
-        maxAge: 30 * 24 * 60 * 60,
-        domain: "chesspecker.org",
+        sameSite: 'lax',
+        path: '/',
+        secure: true, 
       },
     },
   },
-});
-
-export { handler as GET, handler as POST };
+  session: {
+    strategy: 'jwt',
+  },
+};
