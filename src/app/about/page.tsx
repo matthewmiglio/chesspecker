8092f4b29649016e90c492e9a33afba5c1051d0d
Matt <105177840+matthewmiglio@users.noreply.github.com>
2025-05-08 12:19:18 -0400

    removing unused import im about/page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
 import { useThemeAccentColor } from "@/lib/hooks/useThemeAccentColor";
 
 export default function AboutPage() {
