6eddcc1c8b104189750d83fabd481b92f8e151d2
Matthew Miglio <matmigg0804@gmail.com>
2025-06-09 01:12:28 -0400

    not using any

--- src/lib/utils/sortHelpers.ts
+++ src/lib/utils/sortHelpers.ts
@@ -2,20 +2,23 @@ export type SortDirection = "asc" | "desc";
 export type SortType = "number" | "string" | "percent" | "date";
 
 export const sortByType = (
-  a: any,
-  b: any,
+  a: string | number | Date,
+  b: string | number | Date,
   type: SortType,
   direction: SortDirection
-) => {
+): number => {
   const [x, y] = direction === "asc" ? [a, b] : [b, a];
 
   switch (type) {
     case "number":
       return Number(x) - Number(y);
+
     case "percent":
-      return parseFloat(x) - parseFloat(y);
+      return parseFloat(String(x).replace("%", "")) - parseFloat(String(y).replace("%", ""));
+
     case "date":
       return new Date(x).getTime() - new Date(y).getTime();
+
     case "string":
     default:
       return String(x).localeCompare(String(y));
