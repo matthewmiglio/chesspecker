f3aa1444375fe24f530bcb51aef24becc4065017
matt-laptop3 <matmigg0804@gmail.com>
2025-10-13 14:03:47 -0400

    image for not logged in button on the puzzles page

--- src/components/puzzles/not-logged-in-button.tsx
+++ src/components/puzzles/not-logged-in-button.tsx
@@ -1,12 +1,21 @@
 import LoginButton from "@/components/header-footer/LoginButton";
+import Image from "next/image";
 
 export default function NotLoggedInButton() {
   return (
-    <div className="text-red-500 grid-cols-1 flex justify-center items-center">
-      You must sign in to pratice!
-      <div className="px-5">
-        {" "}
-        <LoginButton />
+    <div className="flex flex-col justify-center items-center gap-6">
+      <Image
+        src="/heros/table_study_white.png"
+        alt="Study chess"
+        width={400}
+        height={400}
+        className="rounded-lg opacity-90"
+      />
+      <div className="text-red-500 flex justify-center items-center">
+        You must sign in to pratice!
+        <div className="px-5">
+          <LoginButton />
+        </div>
       </div>
     </div>
   );
